d7b830c99e1be3b960f1a905286d29e03f1fc5c0
Scott Vandehey <fake-scott@spaceninja.com>
2019-08-10 15:38:26 -0700

    lint fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,30 @@
+const path = require('path');
+
 module.exports = {
   // 1. Use the src/index.js file as entry point to bundle it.
   //    If that file imports other JavaScript files, bundle them as well.
-  entry: "./src/index.js",
+  entry: './src/index.js',
   // 2. Tell webpack to use babel-loader for JS files
   module: {
     rules: [
       {
         test: /\.(js)$/,
         exclude: /node_modules/,
-        use: ["babel-loader", "eslint-loader"]
-      }
-    ]
+        use: ['babel-loader', 'eslint-loader'],
+      },
+    ],
   },
   resolve: {
-    extensions: ["*", ".js"]
+    extensions: ['*', '.js'],
   },
   // 3. Put the bundled source code in a bundle.js file in the /dist folder.
   output: {
-    path: __dirname + "/dist",
-    publicPath: "/",
-    filename: "bundle.js"
+    path: path.resolve(__dirname, '/dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
   },
   // 4. Serve our application to the browser from the /dist folder.
   devServer: {
-    contentBase: "./dist"
-  }
+    contentBase: './dist',
+  },
 };
