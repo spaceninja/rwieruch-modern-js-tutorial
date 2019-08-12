6bc40ce0cf6be8860229adf94d30c6459ca18e1f
Scott Vandehey <fake-scott@spaceninja.com>
2019-08-11 21:49:57 -0700

    use relative asset URLs for github pages

--- build-utils/webpack.common.js
+++ build-utils/webpack.common.js
@@ -80,7 +80,7 @@ module.exports = {
   ],
   output: {
     path: path.resolve(__dirname, '../', 'dist'),
-    publicPath: '/',
+    publicPath: '',
     filename: 'bundle.js',
   },
   devServer: {
